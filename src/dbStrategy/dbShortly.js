ea189389b08a3eaedf816ad63bc5de9136d1e929
ana <anapsilva793@gmail.com>
2022-08-08 18:03:20 -0300

    Prepare for deploy

--- src/dbStrategy/dbShortly.js
+++ src/dbStrategy/dbShortly.js
@@ -2,12 +2,14 @@ import pg from 'pg';
 
 const { Pool } = pg;
 
-const connection = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "romelotas",
-  database: "database-shortly",
-});
+
+const databaseConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+}
+
+const connection = new Pool(databaseConfig);
 
 export default connection;
